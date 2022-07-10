fae1134cb5732398f7933ce2489b9b96f313e4ac
CaspaaCaspaa <kasper.kaczmarek@gmail.com>
2022-07-10 23:52:00 +0200

    fies in MeeeintgRestController

--- src/main/java/com/company/enroller/controllers/MeetingRestController.java
+++ src/main/java/com/company/enroller/controllers/MeetingRestController.java
@@ -6,9 +6,8 @@ import com.company.enroller.persistence.ParticipantService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 import com.company.enroller.model.MeetingResponse;
 import com.company.enroller.model.Participant;
 
